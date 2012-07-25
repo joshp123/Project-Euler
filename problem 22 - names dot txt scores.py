d5a75be83d50c330852b1837fb750792dd446a34
Josh Palmer <joshpalmer123@gmail.com>
2012-07-25 19:12:16 +0100

    socreName fixed; TODO: fix file open routine (it splits on "," not "\n")

--- problem 22 - names dot txt scores.py
+++ problem 22 - names dot txt scores.py
@@ -5,31 +5,24 @@ import string
 
 def scoreName(name):
     # score a name based on A = 1, B = 2 etc
-    name = str(name.lower)  # this means we can get indeces easily
-    letter_scores = []
-    for x in xrange(0, len(name) - 1):
-        letter_scores[x] = string.lowercase.index(name[x])
-
-    for x in xrange(0, len(letter_scores) - 1):
-        letter_scores = letter_scores[x] + 1
-
-    score = sum(letter_scores)
+    score = sum([string.lowercase.index(x) + 1 for x in name.lower()])
     return score
 
 
 def main():
-    names = []
+    # names = []
     with open('names.txt', 'rbU') as f:
         names = [line.strip() for line in f]
+    print names
 
     names.sort()
 
-    print names[1:10]
+    print names[0]
 
     for i in xrange(0, len(names) - 1):
         names[i] = scoreName(names[i]) * (i + 1)
 
-    print 'The sum of all the name scores is:' + repr(int(sum(names)))
+    print sum(names)
 
     return
 
